62812d96ef2ca5eadb6d47a4dbcdcc7efff534a3
SiriNguyen <nguyenngocthaiduy.nt@gmail.com>
2022-11-05 22:26:40 +0700

    ss

--- controller/Image.ts
+++ controller/Image.ts
@@ -1,5 +1,5 @@
 const getImage = (req:any, res:any) => {
-    let path = __dirname.replace("\\controller","")
+    let path = __dirname.replace("/controller","")
     res.sendFile(`${path}/public/images/${req.params.Id}`)
 }
 
