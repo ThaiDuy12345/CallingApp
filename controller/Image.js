9b9abba9b464b9b22a07ada4bc1adfa9357a07d6
SiriNguyen <nguyenngocthaiduy.nt@gmail.com>
2022-11-05 19:49:29 +0700

    '

--- controller/Image.js
+++ controller/Image.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const getImage = (req, res) => {
+    console.log(__dirname);
     res.sendFile(`${__dirname}/Images/${req.params.Id}`);
 };
 exports.default = getImage;
--- controller/Image.ts
+++ controller/Image.ts
@@ -1,4 +1,5 @@
 const getImage = (req:any, res:any) => {
+    console.log(__dirname)
     res.sendFile(`${__dirname}/Images/${req.params.Id}`)
 }
 
